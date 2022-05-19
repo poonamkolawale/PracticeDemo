7ae01229ce378ee7d6907f846cfdea5d58a01ff1
poonamkolawale <kpaurnima21@gmail.com>
2022-05-19 14:42:48 +0530

    Practice Demo commit

---
+++ src/practicedemo1.java
@@ -0,0 +1,10 @@
+
+public class practicedemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello welcome to git practice demo ....");
+
+	}
+
+}
